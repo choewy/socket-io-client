6ee90a1d2269176d63b687dab8367dbfd8a32ec3
choewy <choewy32@gmail.com>
2023-12-03 18:27:15 +0900

    feature/app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,52 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, SyntheticEvent, useCallback, useState } from 'react';
+
+import { Box, Tabs, Tab } from '@mui/material';
+
+import { tabProperties } from './tab';
 
 export const App: FunctionComponent = () => {
-  return <div>APP</div>;
+  const [tabIndex, setTabIndex] = useState<number>(0);
+
+  const onChangeTabIndex = useCallback(
+    (_: SyntheticEvent<Element, Event>, value: string | number) => {
+      value = Number(value);
+
+      if (Number.isNaN(value)) {
+        return;
+      }
+
+      setTabIndex(value);
+    },
+    [setTabIndex],
+  );
+
+  return (
+    <>
+      <Box sx={{ width: '100%', position: 'sticky', top: 0, left: 0, background: '#eee', zIndex: 999 }}>
+        <Tabs value={tabIndex} onChange={onChangeTabIndex} centered>
+          {tabProperties.map((property, i) => (
+            <Tab key={property.key} label={property.title} value={i} />
+          ))}
+        </Tabs>
+      </Box>
+
+      <Box sx={{ width: '100%' }}>
+        {tabProperties.map((property, i) => (
+          <Box
+            key={property.key}
+            sx={{
+              display: tabIndex !== i ? 'none' : 'flex',
+              justifyContent: 'center',
+              width: '100%',
+              height: '90vh',
+              padding: 5,
+              boxSizing: 'border-box',
+            }}
+          >
+            {property.component}
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
 };
